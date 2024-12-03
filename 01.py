c3d8f4cd4019a9989ee7551e2594e1111cfd2967
Rüdiger Ranft-Driscoll <git-2024@qzzq.de>
2024-12-03 19:31:57 +0100

    Apply black.

--- 01.py
+++ 01.py
@@ -2,26 +2,33 @@
 
 import sys
 
+
 def read_input_file(name: str):
     with open(name, encoding="ascii") as infile:
         return [tuple(map(int, l.split())) for l in infile]
 
 
-def sort_column(values:list[tuple[int, ...]], col: int) -> list[int]:
+def sort_column(values: list[tuple[int, ...]], col: int) -> list[int]:
     return sorted(map(lambda x: x[col], values))
 
+
 def part1(values) -> int:
-    return sum(map(lambda xy: abs(xy[0]-xy[1]), zip(*map(lambda c: sort_column(values, c), [0, 1]))))
+    return sum(
+        map(
+            lambda xy: abs(xy[0] - xy[1]),
+            zip(*map(lambda c: sort_column(values, c), [0, 1])),
+        )
+    )
 
 
 def part2(values):
     rights = {}
     for _, right in values:
-        rights[right] = rights.get(right, 0)+1
+        rights[right] = rights.get(right, 0) + 1
 
     result = 0
     for left, _ in values:
-        result += rights.get(left, 0)*left
+        result += rights.get(left, 0) * left
 
     return result
 
@@ -31,6 +38,7 @@ def main():
 
     print(part1(values))
     print(part2(values))
+
 
 if __name__ == "__main__":
     main()
